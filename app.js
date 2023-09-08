9444ed7eb63da67e0cf3ad7697c9abe991c587fb
energizet <a2437@yandex.ru>
2023-09-08 18:35:58 +0300

    add get items and item/:id

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 const express = require('express');
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-
-const Response = require("./Response");
 const path = require("path");
 const fs = require("fs");
+
+const Response = require("./Response");
+const ItemController = require('./api/ItemController');
 
 const httpPort = 3030;
 
@@ -17,10 +18,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'http/public')));
 app.use(cors());
 
-app.get("/api/hello", async (req, res) => {
-    sendResponse(res, Response.ok({
-        hello: "world",
-    }));
+app.get("/api/items", async (req, res) => {
+    await interceptHttp(req, res, ItemController.getItems);
+});
+
+app.get("/api/item/:id", async (req, res) => {
+    await interceptHttp(req, res, () => ItemController.getItem(req.params.id));
 });
 
 app.post("/api/hello", express.raw({type: 'application/json'}), async (req, res) => {
@@ -42,6 +45,33 @@ app.listen(httpPort, '0.0.0.0', () => {
     console.log(`HTTP listener started on port ${httpPort}`);
     console.log(`http://localhost:${httpPort}`);
 });
+
+async function interceptHttp(req, res, callback) {
+    try {
+        let result = await callback(req);
+        if (!(result instanceof Response)) {
+            throw result;
+        }
+
+        sendResponse(res, result);
+    } catch (ex) {
+        interceptHttpExceptions(res, ex);
+    }
+}
+
+function interceptHttpExceptions(res, ex) {
+    let error;
+
+    if (ex instanceof Error) {
+        error = Response.error(500, ex.message);
+    } else if (ex instanceof Response) {
+        error = ex;
+    } else {
+        error = Response.error(500, ex);
+    }
+
+    sendResponse(res, error);
+}
 
 function sendResponse(res, response) {
 
